a9bbcf1b2f615b500076def603df2345753ac854
Azamat <azaza7087@gmail.com>
2020-08-16 14:56:36 +0600

    Added pagination

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import *
+from django.core.paginator import Paginator
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 # Create your views here.
@@ -8,7 +9,31 @@ from django.shortcuts import get_object_or_404
 def gun_list(request, name):
     category = Category.objects.get(slug__iexact=name)
     gun_list = Guns.objects.filter(category=category.id)
-    return render(request, 'shop/guns_list.html', context={'gun_list': gun_list, 'name': name})
+
+    paginator = Paginator(gun_list, 3)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ""
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ""
+
+
+    return render(request, 'shop/guns_list.html', context={'gun_list': gun_list,
+                                                           'name': name,
+                                                           'page_object': page,
+                                                           'is_paginated': is_paginated,
+                                                           'prev_url': prev_url,
+                                                           'next_url': next_url
+                                                           })
 
 def main(request):
     return render(request, 'shop/main.html')
@@ -24,3 +49,4 @@ class UserDetail(View):
         user = InfoOfUser.objects.get(login=login)
 
         return render(request, 'shop/user_detail.html', context={'user': user})
+
